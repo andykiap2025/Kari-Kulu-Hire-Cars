ae92823cc5a54c5160e6449df2a15206cc17d601
andykiap2025 <andykiap10@gmail.com>
2025-05-21 13:00:19 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(_file_)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from src.main import app as application
 app =application
