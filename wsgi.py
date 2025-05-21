7729e85cccb3ffa2facb510783eb980963671e22
andykiap2025 <andykiap10@gmail.com>
2025-05-21 13:07:00 +1000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from src.main import app as application
-app =application
+
+# Import the app directly from main.py
+from main import app as application
+
+# For Gunicorn
+app = application
